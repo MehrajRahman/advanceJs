172f867aa076f1c14e90e6dd161e9a4e438f4b52
MehrajRahman <rahmanmehraj627@gmail.com>
2021-02-01 22:45:18 +0600

    done

---
+++ trueFalse.js
@@ -0,0 +1,17 @@
+//Falsy
+//0
+//""
+//undefined
+//null
+//false
+//NaN
+
+//truthy
+//EverythingExcept this two
+let name = 0;
+if(name){
+    console.log("Condition is true");
+}else{
+    console.log("Condition is false");
+
+}
